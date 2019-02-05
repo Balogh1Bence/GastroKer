7c3581b19f7ec136158fa189c851fe72be4644aa
balben <baloghbencefacebook@gmail.com>
2019-02-05 08:05:54 +0100

    dtg

---
+++ login/login/Login.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using login.Services.fleh;
+namespace login
+{
+    public partial class Login : Form
+    {
+        ErrorProvider uname = new ErrorProvider();
+        ErrorProvider pw = new ErrorProvider();
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            us u = new us();
+            Rakt rakt = new Rakt();
+            MessageBox.Show(textBox1.Text);
+            if (textBox1.Text == "" || textBox1.Text == null)
+            {
+                uname.SetError(textBox1, "üres felhasználónév mező");
+            }
+            else
+            {
+                if (u.belep(textBox1.Text, textBox2.Text) == 0)
+                {
+                    rakt.Show();
+
+                }
+
+                if (u.belep(textBox1.Text, textBox2.Text) == 1)
+                {
+                    pw.SetError(textBox2, "helytelen jelszó");
+
+                }
+                else
+                {
+                    pw.Clear();
+
+                    pw.SetError(textBox2, "");
+                }
+                if (u.belep(textBox1.Text, textBox2.Text) == 2)
+                {
+                    uname.SetError(textBox1, "nincs ilyen felhasználónév");
+
+
+                }
+                else
+                {
+                    uname.Clear();
+                    uname.SetError(textBox1, "");
+                }
+                if (u.belep(textBox1.Text, textBox2.Text) == 3)
+                {
+                    pw.SetError(textBox2, "üres jeszó mező");
+
+
+                }
+                else
+                {
+
+                    pw.Clear();
+                    pw.SetError(textBox2, "");
+                }
+            }
+
+        }
+    }
+}
---
+++ login/login/Services/fleh/us.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace login.Services.fleh
+{
+    class us
+    {
+        public string emailOfUser(string username)
+        {
+
+            string name = username;
+            string mail = "";
+            string con = "SERVER=\"localhost\";"
+                + "DATABASE=\"felh\";"
+                + "UID=\"root\";"
+                + "PASSWORD=\"\";"
+                + "PORT=\"3306\";";
+            MySqlConnection connect = new MySqlConnection(con);
+            try
+            {
+                connect.Open();
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e.Message);
+                throw new Exception("Sikertelen adatbázismegnyitás.");
+            }
+
+
+            string query = "SELECT email from deskusers where username like '" + name + "'";
+            try
+            {
+                MySqlCommand cmd = new MySqlCommand(query, connect);
+                MySqlDataReader dr = cmd.ExecuteReader();
+                while (dr.Read())
+                {
+
+                    mail = dr["email"].ToString();
+
+                }
+
+
+                MessageBox.Show(mail);
+
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e.Message);
+
+
+            }
+            return mail;
+        }
+
+        public int belep(string username, string password)
+        {
+            string uname = username;
+
+            string pw = password;
+            string RealPW = null;
+            string con = "SERVER=\"localhost\";"
+               + "DATABASE=\"felh\";"
+               + "UID=\"root\";"
+               + "PASSWORD=\"\";"
+               + "PORT=\"3306\";";
+            MySqlConnection connect = new MySqlConnection(con);
+            try
+            {
+                connect.Open();
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e.Message);
+                throw new Exception("Sikertelen adatbázismegnyitás.");
+            }
+            string query = "SELECT password from deskusers where username='" + uname + "'";
+            MySqlCommand cmd = new MySqlCommand(query, connect);
+            MySqlDataReader dr = cmd.ExecuteReader();
+            string remelemnemures = string.Empty;
+            try
+            {
+
+                while (dr.Read())
+                {
+
+                    RealPW = dr["password"].ToString();
+
+                    remelemnemures = RealPW;
+
+                }
+
+
+
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e.Message);
+
+
+            }
+            /*
+           hibakódok:
+           0: minden jó
+           1: nem jó jelszó
+           2: nincs ilyen felhasználónév
+           3: üres jelszó mező
+           4: üres felhasználónév
+            */
+            if (uname == null)
+            {
+                return 4;
+            }
+            if (RealPW != null)
+            {
+                if (RealPW == pw)
+                {
+                    return 0;
+                }
+            }
+            else return 3;
+            if (string.IsNullOrEmpty(remelemnemures))
+            {
+                return 2;
+            }
+            if (RealPW != pw)
+            {
+                return 1;
+            }
+
+
+
+            return 0;
+        }
+    }
+}
+
