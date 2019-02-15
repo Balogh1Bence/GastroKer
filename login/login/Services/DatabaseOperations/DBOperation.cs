04b5932def58c79a630389c24c1c6bb1b44d06ec
balben <baloghbencefacebook@gmail.com>
2019-02-15 10:27:30 +0100

    mukszik

--- login/login/Services/DatabaseOperations/DBOperation.cs
+++ login/login/Services/DatabaseOperations/DBOperation.cs
@@ -8,6 +8,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace login.Services.DatabaseOperations
 {
@@ -28,20 +29,21 @@ namespace login.Services.DatabaseOperations
             }
             catch (Exception e)
             {
-                System.Windows.Forms.MessageBox.Show("Test");
+           
                 Debug.WriteLine(e.Message);
                 throw new Exception("Sikertelen adatbázismegnyitás.");
             }
+
             DateTime dt = t.getSzavido();
 
-            System.Windows.Forms.MessageBox.Show(dt.ToShortDateString());
+           
             string st = dt.ToShortDateString();
-            
+           
             st=st.Replace('.', '-');
             st=st.TrimEnd('-');
          
 
-            System.Windows.Forms.MessageBox.Show(st);
+           
             string uj = "";
             int i = 0;
             while (i < st.Length)
@@ -55,14 +57,18 @@ namespace login.Services.DatabaseOperations
 
                 i++;
             }
-            System.Windows.Forms.MessageBox.Show(uj);
+           
 
-            string query = "UPDATE `termekek` SET `Tkod`="+t.getTkod()+",`Tnev`="+t.getTNev()+",`Tar`="+t.getTar()+",`Tkeszl`="+t.getTkeszl()+",`Tmert`="+t.getMert()+",`Tkatkod`="+t.getTkatkod()+",`Tvonkod`="+t.getTvonkod()+",`Tszavido`="+uj+",`Tegalizalte`="+t.getTegalizalte()+" WHERE id="+id+"";
-            System.Windows.Forms.MessageBox.Show(query);
+            string query = "UPDATE `termekek` SET `Tkod`="+t.getTkod()+",`Tnev`='"+t.getTNev()+"',`Tar`="+t.getTar()+",`Tkeszl`="+t.getTkeszl()+",`Tmert`='"+t.getMert()+"',`Tkatkod`="+t.getTkatkod()+",`Tvonkod`="+t.getTvonkod()+",`Tszavido`='"+uj+"',`Tegalizalte`="+t.getTegalizalte()+" WHERE Tkod="+id+"";
+           //a lekérdezés jó. máshol van a hiba
+            
+            
+           
             try
             {
-                MySqlCommand cm = new MySqlCommand(query);
+                MySqlCommand cm = new MySqlCommand(query, connect);
                 cm.ExecuteNonQuery();
+                MessageBox.Show(connect.State + "");
 
             }
             catch (Exception e)
