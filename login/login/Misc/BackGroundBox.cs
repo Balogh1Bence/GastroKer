d21c78050a658549794da81918f6e3c065aca09e
balben <baloghbencefacebook@gmail.com>
2019-02-21 10:40:44 +0100

    started creating backgroundComponent

---
+++ login/login/Misc/BackGroundBox.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace login.Misc
+{
+    public class BackGroundBox : PictureBox
+    {
+        bool isPlaceHolder = true;
+        string _ImgName;
+        public string ImgName
+        {
+            get { return _ImgName; }
+            set
+            {
+                _ImgName = value;
+                setImg();
+            }
+        }
+        public
+
+     
+
+        
+
+        void setImg()
+        {
+
+            Image = Image.FromFile(ImgName);
+              
+        }
+
+         void setTransparency()
+         {   
+
+         }
+         void setSizes()
+         {
+            Height =;
+            Width =;
+         }
+        
+   
+      
+
+       
+    }
+}
