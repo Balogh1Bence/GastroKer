c8e6ed1d0ce8dc746cfd11a006974c09a93b7475
balben <baloghbencefacebook@gmail.com>
2019-03-22 11:36:16 +0100

    asd

--- login/login/Rakt.cs
+++ login/login/Rakt.cs
@@ -40,7 +40,7 @@ namespace login
             placeHolderTextBox1.setPlaceHolder("keresés név alapján");
 
             const string REGISTRY_KEY = @"HKEY_CURRENT_USER\MyApplication";
-            const string REGISTY_VALUE = "secondRun";
+            const string REGISTY_VALUE = "firstRun";
             if (Convert.ToInt32(Microsoft.Win32.Registry.GetValue(REGISTRY_KEY, REGISTY_VALUE, 0)) == 0)
             {
 
@@ -54,6 +54,7 @@ namespace login
                         aru.addNewUser();
 
                         cst = new connectionSetter();
+                        cst.ShowDialog();
                         if (cst.DialogResult == DialogResult.OK)
                         {
                             cst.setCons();
