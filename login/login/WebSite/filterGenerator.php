7fa2d9a7419e95041271bb3fb882c91ee6be5aad
balben <baloghbencefacebook@gmail.com>
2019-03-22 11:26:32 +0100

    mukszik mindenhol....

--- login/login/WebSite/filterGenerator.php
+++ login/login/WebSite/filterGenerator.php
@@ -3,10 +3,11 @@ require_once("connectG.php");
 $sql="select * from kat";
 $result = $connect->query($sql);
 $html="";
-$selection="<select>";
+$selection="<select onChange='loadNewContent(value)'>";
+$selection.="<option>kérlek válassz</option>";
 while($row = $result->fetch_assoc()) {/*
 $html.="<input type='button' id=".$row['Tkatkod']." value=".$row['Tkatnev']." onclick='loadNewContent(this.id)'>";*/
-$selection.="<option id='".$row['Tkatkod']."', onclick='loadNewContent(this.id)'>".$row['Tkatnev']."</option>";
+$selection.="<option value='".$row['Tkatkod']."'>".$row['Tkatnev']."</option>";
 
 }
 $selection.="</select>";
