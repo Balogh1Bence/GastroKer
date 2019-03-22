9ca7fa16af9eef235ae4d4df2796fdedf68430d3
balben <baloghbencefacebook@gmail.com>
2019-03-22 12:47:01 +0100

    asd

--- login/login/WebSite/filterGenerator.php
+++ login/login/WebSite/filterGenerator.php
@@ -3,9 +3,12 @@ require_once("connectG.php");
 $sql="select * from kat";
 $result = $connect->query($sql);
 $html="";
-while($row = $result->fetch_assoc()) {
-$html.="<input type='button' id=".$row['Tkatkod']." value=".$row['Tkatnev']." onclick='loadNewContent(this.id)'>";
-}
+$selection="<select>";
+while($row = $result->fetch_assoc()) {/*
+$html.="<input type='button' id=".$row['Tkatkod']." value=".$row['Tkatnev']." onclick='loadNewContent(this.id)'>";*/
+$selection.="<option id=".$row['Tkatkod'].", onclick='loadNewContent(this.id)'>".$row['Tkatnev']."</option>";
 
-echo $html;
+}
+$selection.="</select>";
+echo $selection;
 ?>
--- login/login/WebSite/szur.php
+++ login/login/WebSite/szur.php
@@ -2,8 +2,10 @@
 require_once("connectG.php");
 if (isset($_POST['kat'])){
 $kat=$_POST['kat'];
-$sql = "SELECT Tnev, Tkeszl, Tar, Tkod FROM `kat`, termekek WHERE kat.Tkatkod=termekek.Tkatkod and termekek.Tkatkod=".$kat."";
+$sql = "SELECT Tnev, Tkeszl, Tar, Tkod FROM kat, termekek WHERE kat.Tkatkod=termekek.Tkatkod and termekek.Tkatkod=".$kat."";
 	$result = $connect->query($sql);
+	echo $sql;
+	echo $connect->error;
 $table="<table id='table' class='table'><tr><th>termék neve</th><th>raktár</th><th>termék ára</th><th></th><th id='mennyiseg'></th></tr>";
 while($row = $result->fetch_assoc()) {
 $table.="<tr><td>{$row['Tnev']}</td><td>{$row['Tkeszl']}</td><td>{$row['Tar']}</td><td><button id='{$row['Tkod']}' class='btn'  onclick='oneless(this.id, {$row['Tar']})'>-</button></td><td><button class='btn'  id='{$row['Tkod']}' onclick='onemore(this.id, {$row['Tar']})'>+</button></td><td><span  cols='1'  name='{$row['Tkod']}' class='{$row['Tkod']}'>0</span></td></tr>";
