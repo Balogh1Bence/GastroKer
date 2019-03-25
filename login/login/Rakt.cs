edf103f020816c10fa9b546479ada7ecb7cf468e
balben <baloghbencefacebook@gmail.com>
2019-03-25 06:49:40 +0100

    asd

--- login/login/Rakt.cs
+++ login/login/Rakt.cs
@@ -41,7 +41,7 @@ namespace login
             placeHolderTextBox1.setPlaceHolder("keresés név alapján");
 
             const string REGISTRY_KEY = @"HKEY_CURRENT_USER\MyApplication";
-            const string REGISTY_VALUE = "xthRun";
+            const string REGISTY_VALUE = "xtxhRun";
             if (Convert.ToInt32(Microsoft.Win32.Registry.GetValue(REGISTRY_KEY, REGISTY_VALUE, 0)) == 0)
             {
 
