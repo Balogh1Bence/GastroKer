d63011539a34386976c6a242575b1999a8b676be
balben <baloghbencefacebook@gmail.com>
2019-03-25 10:39:18 +0100

    mukszik a web

--- login/login/WebSite/loadall.php
+++ login/login/WebSite/loadall.php
@@ -1,13 +1,18 @@
-﻿<?php
+﻿<html>
+<head>
+ <link rel="stylesheet" href="css/bootstrap-4.3.1-dist/css/bootstrap.css"/>
+</head>
+</html>
+<?php
 require_once("connectG.php");
 if (isset($_POST['us'])){
 $sql = "SELECT * from termekek";
 	$result = $connect->query($sql);
-$table="<table id='table' class='table'><tr><th>termék neve</th><th>raktár</th><th>termék ára</th><th></th><th id='mennyiseg'></th></tr>";
+$table="<div class='table-responsive'><table id='table' class='table  table-striped'><thead><tr><th>termék neve</th><th>raktár</th><th>termék ára</th><th></th><th id='mennyiseg'></th></tr></thead><tbody>";
 while($row = $result->fetch_assoc()) {
 $table.="<tr><td>{$row['Tnev']}</td><td>{$row['Tkeszl']}</td><td>{$row['Tar']}</td><td><button id='{$row['Tkod']}' class='btn'  onclick='oneless(this.id, {$row['Tar']})'>-</button></td><td><button class='btn'  id='{$row['Tkod']}' onclick='onemore(this.id, {$row['Tar']})'>+</button></td><td><span  cols='1'  name='{$row['Tkod']}' class='{$row['Tkod']}'>0</span></td></tr>";
     }
-$table.="</table>";
+$table.="</tbody></table></div>";
 echo $table;
     /*
 	$pw=$_POST['pw'];	
